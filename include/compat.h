df998efee3402de226a4e96f70be19df087425a4
Mitchell Clough <maclough@bu.edu>
2022-10-22 09:35:18 -0400

    fix some msvc compatibility

--- include/compat.h
+++ include/compat.h
@@ -12,37 +12,27 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <float.h>
-
+#include <stdint.h>
 
 // For Microsoft Visual Studio
 #ifdef _MSC_VER
-
-typedef signed char int8_t; 
-typedef signed short int16_t; 
-typedef signed long int32_t; 
-typedef unsigned char uint8_t; 
-typedef unsigned short uint16_t; 
-typedef unsigned long uint32_t; 
-typedef unsigned long long uint64_t;
 
 #ifndef log2                          // no log2 in xp
 #define log2(x) (log(x)/log(2))       // there are fast tricks for finding int(log2(x))
 #endif                             
 
-#define  //_inline
 #define msvcextern extern
 
 #define hypotf _hypotf
 
- long int lround(float x);
- double round(double x);
- float roundf(float x);
+// long int lround(float x);
+// double round(double x);
+// float roundf(float x);
 
-#define isnan _isnan
+//#define isnan _isnan
 
 #else  // NOT MSVC
 
-#include <stdint.h>
 #define msvcextern 
 #ifndef __declspec
 #define __declspec(x) 
